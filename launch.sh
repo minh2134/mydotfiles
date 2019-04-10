c714fe508d3ecccb1e71fbf6339019d68023c1b9
Minh Pham <minh213456@hotmail.com>
2019-04-10 02:04:15 -0400

    add script to automate alacritty config

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This script creates symlink using the config files in the repo as a base.
+#Any error with mkdir can be safely disregarded
+
+#some interesting variables
+
+SOURCE="$HOME/mydotfiles/config"
+TAR="$HOME/.config"
+
+#alacritty
+if [ ! -d "$TAR/alacritty" ]; then
+	mkdir $TAR/alacritty
+fi
+
+ln -sf $SOURCE/alacritty/alacritty.yml $TAR/alacritty/alacritty.yml 
